5bfb99c8e7978ad42120e9638df2ad6222d018ce
John Smoker <testgits@yandex.ru>
2021-04-06 14:13:41 +0500

    first commit

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,27 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static helpers.AttachmentHelper.*;
+
+public class TestBase {
+    @BeforeAll
+    static void setup() {
+        addListener("AllureSelenide", new AllureSelenide());
+        Configuration.browserSize = "1024x768";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @AfterEach
+    void afterEach() {
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachAsText("Browser console logs", getConsoleLogs());
+        closeWebDriver();
+    }
+}
